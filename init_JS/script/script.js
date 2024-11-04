7ecaa9385dde1618b6d2737abe891b3920ee28dd
Ababacar Samb <aba.sam@dwwm.evx>
2024-11-04 17:56:52 +0100

    exos js

---
+++ init_JS/script/script.js
@@ -0,0 +1,93 @@
+
+// let compteur=2;
+
+// function maFonction()
+
+// {
+//     let myVar =456;
+//     console.log("myVar:"+myVar);
+    
+//     console.log("myVar fonction :"+myVar);
+// }
+
+
+
+// if(compteur>1)
+
+// {
+// let myVar2="Wazaa!";
+// console.log("myVar2:"+myVar2);
+// }
+
+// console.log("myVar fin:"+myVar);
+
+
+/*la methode write ()*/
+//let age=35;
+//document.write(age);
+//document.write("Votre age est"+ age);
+//document.write("<b>"+"Votre age est"+"</b>" +age);
+//document.write("<b>"+"Votre age est"+"</b>"+age);
+
+
+/*la Methode Alert()
+myVar="Bonjour";
+window.alert(myVar);
+window.alert("chaine de caractères");
+windows.alert(myVar+"chaine de caractères");
+*/
+
+
+/*Methode prompt()*/
+/*let reponse1=window.prompt("Saisissez votre nom");
+let reponse2=window.prompt("Saisissez votre prénom");
+let sexe;
+
+
+
+/*la methode confirme()
+if(window.confirm("Etes-vous un homme?")==true)
+{
+    sexe="Mr";
+}
+else{
+    sexe= "Mme";
+}
+alert("Bonjour "+ sexe + " " + reponse2 + " " + reponse1 + ". Bienvenue sur notre site web!");
+*/
+
+
+
+/* les operateurs de calcul Jscript
+
+let x=11;
+let addition= x+3;//14
+console.log(x+3);
+
+let x=11;
+let difference=x-3;//11
+console.log(x-3);
+
+
+let x=11;
+let product=x*2;//22
+console.log(x*2);
+
+let x=11;
+let quotient=x/2;//5.5
+console.log(x/2);
+
+
+let x=11;
+let remainder=5%2;//1
+console.log(x%2);
+*/
+//demander un nombre  à l'utilisateur
+let nombre=prompt("Veuillez entrer un nombre:");
+//Verifier si le nombre est pair ou impair
+if(nombre %2===0)
+{
+    console.log("nombre pair");
+}else{
+    console.log("nombre impair");
+}
